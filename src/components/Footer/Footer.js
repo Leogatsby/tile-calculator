47fed2fe3d86d907648143814887421aa503d7d9
unknown <100k1@naver.com>
2025-05-28 13:12:27 +0900

    풋터 수정

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,7 @@ function Footer() {
     <footer className="footer">
       <div className="footer-container">
         <p>&copy; 2025 타일 계산기. All rights reserved.</p>
+        <p>청년타일 010-3375-3948 대표자 백광일</p>
       </div>
     </footer>
   );
